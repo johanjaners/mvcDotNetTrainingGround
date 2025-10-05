eff966161f48c2dd1618c17068d6c570332f1f6c
Johan Janérs <johanjaners@gmail.com>
2025-10-05 11:39:54 +0200

    DevelopersCOntroller: Add action method Index

--- Controllers/DevelopersController.cs
+++ Controllers/DevelopersController.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using mvcDotNetTrainingGround.Models; // Db
-
 namespace mvcDotNetTrainingGround.Controllers;
-
 public class DeveloperControllers : Controller
 {
     private Db _db;
-
     public DeveloperControllers(Db db)
     {
         _db = db;
     }
+    public IActionResult Index() => View(_db.Developers);
 }
