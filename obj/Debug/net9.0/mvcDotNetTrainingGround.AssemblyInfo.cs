b7cb4a4c630693c74e3bd568e4dc4a568cf858a4
Johan Janérs <johanjaners@gmail.com>
2025-10-05 18:24:03 +0200

    Views/Devs: added Create.cshtml: added form using tag helpers + button

--- obj/Debug/net9.0/mvcDotNetTrainingGround.AssemblyInfo.cs
+++ obj/Debug/net9.0/mvcDotNetTrainingGround.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvcDotNetTrainingGround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7576f29c641687fe11cd5743be3726828f337c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8832d6c110d9e16958806781c8f521067a2aa27")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvcDotNetTrainingGround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvcDotNetTrainingGround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
